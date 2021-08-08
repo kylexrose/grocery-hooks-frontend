4498e5c89f0e0c30ae516aa94ac0a1920ebc9533
kylexrose@gmail.com <kylexrose@gmail.com>
2021-08-08 18:59:41 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@
 import './App.css';
 import GroceryList from './components/GroceryList/GroceryList';
 import {GroceryContext} from './Contexts/context'
+import Axios from './components/utils';
+
 
 function App() {
+  let groceryList = [];
+
+  useEffect(() => {
+    
+  }, [input])
 
   function showGroceryList(){
     return(
-      <GroceryContext.Provider value={{yes: "true"}}>
+      <GroceryContext.Provider value={{}}>
         <GroceryList/>
       </GroceryContext.Provider>
     )
     
+  }
+
+  async function getAllGroceries(){
+    try{
+      allGroceries = await Axios.get('/api/groceries/get-all-groceries')
+    }catch(e){
+      console.log(e)
+    }
   }
   return (
     <div className="App">
